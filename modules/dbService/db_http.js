2c1849568482ea6285a9687aaa80387eff12c7c6
haoxuebing <haoxuebing@iyunxiao.com>
2017-11-08 13:39:00 +0800

    添加手机号

--- modules/dbService/db_http.js
+++ modules/dbService/db_http.js
@@ -19,11 +19,13 @@ function InsertSocket(socket, status, cb) {
     var userinfo = socket.handshake.headers.referer.split('?')[1].split('&');
     var headPic = userinfo[0].split('=')[1];
     var userName = userinfo[1].split('=')[1];
+    var userMobile = userinfo[2].split('=')[1];
     var userAddress = socket.handshake.address;
 
     var postyData = {
         CreatedTime: new Date().toLocaleString(),
         UserName: decodeURI(userName),
+        UserMobile: userMobile,
         HeadPicture: headPic,
         UserAddress: userAddress,
         UserAgent: JSON.stringify(socket.handshake.headers).split('user-agent":"')[1],
